8553f8fb493bd3b262c0e65d91f58f8fe1f98e36
geoDwarf <fedo.baiocco@gmail.com>
2020-05-20 20:32:56 +0100

    added healthcheck at start up

--- src/main/java/com/geodwarf/apollo/Application.java
+++ src/main/java/com/geodwarf/apollo/Application.java
@@ -3,19 +3,32 @@ package com.geodwarf.apollo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.ResourceAccessException;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application  implements CommandLineRunner
+{
+
+    @Autowired
+    private  InitHealthCheck initHealthCheck;
 
     public static void main(String[] args) {
-        InitHealthCheck intialHealthCheck = new InitHealthCheck();
-        Logger logger = LoggerFactory.getLogger(Application.class);
         SpringApplication.run(Application.class, args);
-        logger.info("Happy days");
-        intialHealthCheck.healthCheck();
     }
+
+    @Override
+    public void run(String...args) throws Exception {
+        initHealthCheck.healthCheck();
+    }
+
+    //TODO use a configuration file
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
--- src/main/java/com/geodwarf/apollo/InitHealthCheck.java
+++ src/main/java/com/geodwarf/apollo/InitHealthCheck.java
@@ -2,24 +2,31 @@ package com.geodwarf.apollo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class InitHealthCheck {
+    //TODO check if it can be injected or used in a different way
     Logger logger = LoggerFactory.getLogger(InitHealthCheck.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public  void  healthCheck() throws ResourceAccessException {
         try{
+            //TODO put in a properties file
             final String url = "http://localhost:8081/areyoualive";
-            RestTemplate restTemplate = new RestTemplate();
+            final String actuatorUrl = "http://localhost:8081/actuator/health";
             String response = restTemplate.getForObject(url, String.class);
+            String responseActuator = restTemplate.getForObject(actuatorUrl, String.class);
             logger.info("is the backend alive: "+response);
+            logger.info("is the backend alive calling the actuator: "+responseActuator);
         }catch(ResourceAccessException e){
             logger.info("Back end unavailable!!");
+        }
     }
 
-
-    }
 }
