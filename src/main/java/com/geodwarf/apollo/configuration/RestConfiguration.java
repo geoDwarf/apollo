5a6ab40a64054984058a9b182fab99b1becf3533
geoDwarf <fedo.baiocco@gmail.com>
2020-05-28 21:36:45 +0100

    added properties and configuration file

---
+++ src/main/java/com/geodwarf/apollo/configuration/RestConfiguration.java
@@ -0,0 +1,23 @@
+package com.geodwarf.apollo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public URI uri(@Value("${url.basic}")String basicUrl, @Value("${url.health.actuator}") String actuatorUrl )throws URISyntaxException {
+        return new URI(basicUrl+actuatorUrl);
+    }
+}
--- src/test/java/com/geodwarf/apollo/InitHealthCheckTest.java
+++ src/test/java/com/geodwarf/apollo/InitHealthCheckTest.java
@@ -1,12 +1,12 @@
 package com.geodwarf.apollo;
 
+import com.geodwarf.apollo.utils.InitHealthCheck;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import java.net.URI;
@@ -14,30 +14,33 @@ import java.net.URI;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-@ComponentScan("com.geodwarf")
 public class InitHealthCheckTest {
-
-    @Autowired
+    //TODO add Exception test for not 200 http status code
+    @Mock
     private URI uri;
-
     @Mock
     private RestTemplate restTemplate;
     @Mock
     private ResponseEntity<String> responseEntity;
 
     @InjectMocks
-    private  InitHealthCheck initHealthCheck;
+    private InitHealthCheck initHealthCheck;
 
     @Test
     public void initHealthCheckIsCalledWithExpectedParameters(){
 
         //Given
+        when(uri.getHost()).thenReturn("localhost");
+        when(uri.getPath()).thenReturn("actuator/health");
         when(restTemplate.getForEntity(uri,String.class)).thenReturn(responseEntity);
+        when(responseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
+        when(responseEntity.getBody()).thenReturn("{status:UP}");
 
         //When
         initHealthCheck.healthCheck();
 
         //Then
+        verify(restTemplate,times(1)).getForEntity(uri,String.class);
         verify(responseEntity, times(1)).getBody();
         verify(responseEntity, times(1)).getStatusCode();
 
