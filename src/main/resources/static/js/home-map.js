e92b0f6311e78a5dcaecac4816b149a647874679
geoDwarf <fedo.baiocco@gmail.com>
2020-08-17 21:30:33 +0100

    fixed bootstrap import and added working geoJson sample

--- src/main/resources/static/js/home-map.js
+++ src/main/resources/static/js/home-map.js
@@ -1,7 +1,24 @@
 
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([42.0505, 12.09], 10);
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [12.09, 42.0505]
+    }
+};
 
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -11,3 +28,7 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 		tileSize: 512,
 		zoomOffset: -1
 	}).addTo(mymap);
+
+	L.geoJSON(geojsonFeature, {
+                                  style: myStyle
+                              }).addTo(mymap);
